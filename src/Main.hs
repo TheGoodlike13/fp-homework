8feb3b66ec7e798220a7c5d82da8c2d45b343a61
Goodlike <henrisssss@hotmail.com>
2015-10-22 01:15:28 +0300

    Implemented desired functions using v3

---
+++ src/Main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Data.Maybe
+import JsonParser
+import TicTacToe
+
+validate :: String -> Bool
+validate json = fromMaybe False (fmap isValid (parsedMoves json))
+
+move :: String -> Maybe (Int, Int, Char)
+move json = parsedMoves json >>= replay >>= nextMove >>= convert
+
+winner :: String -> Maybe Char
+winner json = fmap convertToken (parsedMoves json >>= replay >>= gameWinner)
+
+parsedMoves :: String -> Maybe MoveHistory
+parsedMoves = parseJson
+
+convert :: Move -> Maybe (Int, Int, Char)
+convert (x, y, token)
+    | x < 0 || x > 2 || y < 0 || y > 2 = Nothing
+    | otherwise = Just (x, y, convertToken token)
+
+convertToken :: Token -> Char
+convertToken token = head (show token)
