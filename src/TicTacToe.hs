5ccbd20e2bdf9f6d1c4d619c97675df7821b7c0c
Goodlike <henrisssss@hotmail.com>
2015-10-21 23:14:36 +0300

    v3

--- src/TicTacToe.hs
+++ src/TicTacToe.hs
@@ -1,2 +1,38 @@
 module TicTacToe where
 
+import Data.List
+import Data.Maybe
+import JsonParser
+
+data Token = Cross | Circle deriving (Eq)
+data Move = Move (Int, Int, Token) deriving (Eq)
+data MoveHistory = MoveHistory [Move] deriving (Eq)
+data Board = Board [[Maybe Token]] deriving (Eq)
+
+instance Show Token where
+    show Cross = "X"
+    show Circle = "O"
+
+instance Show Move where
+    show (Move (x, y, token)) = "(" ++ show x ++ ", " ++ show y ++ ", " ++ show token ++ ")"
+
+instance Show MoveHistory where
+    show (MoveHistory moves) = "[" ++ intercalate ", " [show move | move <- moves] ++ "]"
+
+instance Show Board where
+    show (Board board) = showBoard board
+    show (Board []) = "\n+-----+"
+    show (Board (row : others)) = "\n+-----+\n" ++ show row ++ show (Board others)
+
+showBoard :: [[Maybe Token]] -> String
+showBoard [] = "\n+-----+"
+showBoard (row : others) = "\n+-----+\n" ++ rowString ++ showBoard others
+    where
+        rowString = case row of
+            [] -> "|"
+            (Nothing : others) ->
+
+
+
+
+
